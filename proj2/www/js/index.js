1b81f712dc7b5b5e891de50c26d113ea31f989bc
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-10-21 16:12:47 +0200

    Versió final

--- proj2/www/js/index.js
+++ proj2/www/js/index.js
@@ -1,15 +1,82 @@
 $(function() {
-    var dialog, form,
-        taskName = $("#task-name");
+    var dialog, editDialog, form, editForm, taskName, editTaskName, currentTask;
 
+    taskName = $("#task-name");
+    editTaskName = $("#edit-task-name");
+
+    // Funció per carregar tasques del Local Storage
+    function loadTasks() {
+        var tasks = JSON.parse(localStorage.getItem("tasklist")) || [];
+
+        // Si no hi ha tasques al Local Storage, afegim les tasques predeterminades només un cop
+        if (tasks.length === 0) {
+            tasks = ["FER LA COMPRA", "ACABAR EXERCICI TASKLIST", "TRUCAR AL METGE"];
+            localStorage.setItem("tasklist", JSON.stringify(tasks)); // Desa les tasques predeterminades
+        }
+
+        // Afegeix les tasques desades al Local Storage al DOM
+        tasks.forEach(function(task) {
+            addTaskToDOM(task);
+        });
+    }
+
+    // Funció per desar la llista de tasques al Local Storage
+    function saveTasks() {
+        var tasks = [];
+        $("#tasklist li").each(function() {
+            tasks.push($(this).find(".task-text").text());
+        });
+        localStorage.setItem("tasklist", JSON.stringify(tasks));
+    }
+
+    // Funció per afegir una nova tasca al DOM i al Local Storage
     function addTask() {
         var task = taskName.val();
         if (task) {
-            $("#tasklist").append("<li>" + task + "</li>");
+            addTaskToDOM(task); // Afegeix al DOM
+            saveTasks(); // Desa al Local Storage
         }
         dialog.dialog("close");
     }
 
+    // Funció per afegir una tasca al DOM
+    function addTaskToDOM(task) {
+        var taskItem = $("<li>")
+            .append("<span class='task-text'>" + task + "</span>")
+            .append("<button class='edit-btn'>edit</button>")
+            .append("<button class='delete-btn'>X</button>");
+
+        $("#tasklist").append(taskItem);
+        attachTaskActions(taskItem); // Adjunta els esdeveniments a les tasques
+    }
+
+    // Funció per editar una tasca existent
+    function editTask() {
+        var newTaskName = editTaskName.val();
+        if (newTaskName) {
+            currentTask.find(".task-text").text(newTaskName);
+            saveTasks(); // Desa els canvis al Local Storage
+        }
+        editDialog.dialog("close");
+    }
+
+    // Funció per adjuntar esdeveniments als botons d'edició i eliminació
+    function attachTaskActions(taskItem) {
+        // Botó d'edició
+        taskItem.find(".edit-btn").on("click", function() {
+            currentTask = $(this).closest("li");
+            editTaskName.val(currentTask.find(".task-text").text());
+            editDialog.dialog("open");
+        });
+
+        // Botó d'eliminació
+        taskItem.find(".delete-btn").on("click", function() {
+            $(this).closest("li").remove();
+            saveTasks(); // Desa després d'eliminar la tasca
+        });
+    }
+
+    // Inicialització del diàleg per afegir tasques
     dialog = $("#dialog-form").dialog({
         autoOpen: false,
         modal: true,
@@ -24,12 +91,38 @@ $(function() {
         }
     });
 
+    // Inicialització del diàleg per editar tasques
+    editDialog = $("#dialog-edit-form").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Guardar": editTask,
+            Cancelar: function() {
+                editDialog.dialog("close");
+            }
+        },
+        close: function() {
+            editForm[0].reset();
+        }
+    });
+
+    // Captura del formulari d'afegir tasques
     form = dialog.find("form").on("submit", function(event) {
         event.preventDefault();
         addTask();
     });
 
+    // Captura del formulari d'editar tasques
+    editForm = editDialog.find("form").on("submit", function(event) {
+        event.preventDefault();
+        editTask();
+    });
+
+    // Obrir el diàleg d'afegir tasca quan es clica el botó
     $("#add-task").button().on("click", function() {
         dialog.dialog("open");
     });
+
+    // Carrega la llista de tasques quan es carrega la pàgina
+    loadTasks();
 });
