7f143b110d596d235016a4acef1657ef290c318e
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-10-21 15:55:19 +0200

    Versió bàsica

--- proj2/www/js/index.js
+++ proj2/www/js/index.js
@@ -1,58 +1,15 @@
 $(function() {
-    var dialog, editDialog, form, editForm, taskName, editTaskName, currentTask;
+    var dialog, form,
+        taskName = $("#task-name");
 
-    taskName = $("#task-name");
-    editTaskName = $("#edit-task-name");
-
-    // Funció per afegir una tasca nova
     function addTask() {
         var task = taskName.val();
         if (task) {
-            // Creem un nou element de la llista amb el text i els botons
-            var taskItem = $("<li>")
-                .append("<span class='task-text'>" + task + "</span>")
-                .append("<button class='edit-btn'>edit</button>")
-                .append("<button class='delete-btn'>X</button>");
-
-            // Afegim el nou element a la llista
-            $("#tasklist").append(taskItem);
-
-            // Adjunta els esdeveniments d'edició i eliminació als botons
-            attachTaskActions(taskItem);
+            $("#tasklist").append("<li>" + task + "</li>");
         }
-
-        // Tanquem el diàleg i reiniciem el formulari
         dialog.dialog("close");
     }
 
-    // Funció per editar una tasca existent
-    function editTask() {
-        var newTaskName = editTaskName.val();
-        if (newTaskName) {
-            // Actualitza el text de la tasca
-            currentTask.find(".task-text").text(newTaskName);
-        }
-
-        // Tanquem el diàleg d'edició
-        editDialog.dialog("close");
-    }
-
-    // Funció per adjuntar esdeveniments als botons d'edició i eliminació
-    function attachTaskActions(taskItem) {
-        // Botó d'edició
-        taskItem.find(".edit-btn").on("click", function() {
-            currentTask = $(this).closest("li");
-            editTaskName.val(currentTask.find(".task-text").text());
-            editDialog.dialog("open");
-        });
-
-        // Botó d'eliminació
-        taskItem.find(".delete-btn").on("click", function() {
-            $(this).closest("li").remove();
-        });
-    }
-
-    // Inicialitzem el diàleg per afegir tasques
     dialog = $("#dialog-form").dialog({
         autoOpen: false,
         modal: true,
@@ -67,40 +24,12 @@ $(function() {
         }
     });
 
-    // Inicialitzem el diàleg per editar tasques
-    editDialog = $("#dialog-edit-form").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            "Guardar": editTask,
-            Cancelar: function() {
-                editDialog.dialog("close");
-            }
-        },
-        close: function() {
-            editForm[0].reset();
-        }
-    });
-
-    // Captura del formulari d'afegir tasques
     form = dialog.find("form").on("submit", function(event) {
         event.preventDefault();
         addTask();
     });
 
-    // Captura del formulari d'editar tasques
-    editForm = editDialog.find("form").on("submit", function(event) {
-        event.preventDefault();
-        editTask();
-    });
-
-    // Obrir el diàleg d'afegir tasca quan es clica el botó
     $("#add-task").button().on("click", function() {
         dialog.dialog("open");
-    });
-
-    // Adjunta esdeveniments a les tasques existents
-    $("#tasklist li").each(function() {
-        attachTaskActions($(this));
     });
 });
